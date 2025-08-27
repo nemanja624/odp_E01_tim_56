3bdf93acf498da95687f6261153bd4173f3cf1b7
Nemanja Mirkovic <nemanja.nm123@gmail.com>
2025-08-27 18:22:40 +0200

    Update api.ts

--- client/src/api.ts
+++ client/src/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const API_BASE =
   import.meta.env.VITE_API_BASE || 'http://localhost:4000/api';
 
-export const SERVER_BASE = API_BASE.replace(/\/api$/, ''); // npr. http://localhost:4000
+export const SERVER_BASE = API_BASE.replace(/\/api$/, ''); 
 
 export const api = axios.create({
   baseURL: API_BASE,
